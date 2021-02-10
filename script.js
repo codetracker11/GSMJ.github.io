822bcb496a713c634f1550c42cc59b544e647fca
codetracker11 <70073327+codetracker11@users.noreply.github.com>
2021-02-11 02:21:52 +0530

    update2

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ function showSlides(n) {
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main").style.hide();
+  
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
